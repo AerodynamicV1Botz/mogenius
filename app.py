7882bb19ff2c101d4da1349cad3432bb3a71d835
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-12-06 11:14:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello, World!'
+
+#Ex https://AerodynamicV1Botz:ghp_147bkkabcdefgh@github.com/AerodynamicV1Botz/anything
+os.system("git clone https://AerodynamicV1Botz:token@github.com/AerodynamicV1Botz/AeroXManagement. okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 m- AeroXManagement. &")
