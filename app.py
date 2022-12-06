82e5569b151b22f3822569974a960e181d454919
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-12-06 11:43:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-#Ex https://AerodynamicV1Botz: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@github.com/AerodynamicV1Botz/AeroXManagement.
-os.system("git clone https://AerodynamicV1Botz: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@github.com/AerodynamicV1Botz/AeroXManagement. okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 m- AeroXManagement. &")
+#Ex https://AerodynamicV1Botz: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@github.com/AerodynamicV1Botz/VrajeshBotz
+os.system("git clone https://AerodynamicV1Botz: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@github.com/AerodynamicV1Botz/VrajeshBotz okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m VrajeshBotz &")
